41929aaecda4dbbcf7bbf5abad077897385bb3a3
SyedSaudAli-gh <saud.saleem93@gmail.com>
2025-05-07 17:46:56 +0500

    update

--- 💯 100_days_with_harry/exercise3.py
+++
@@ -1,8 +0,0 @@
-input = input("Enter your Name: ")
-print("Hello my name is:",input)
-
-x = input("Enter the First number:")
-y = input("Enter the Second number:")
-print(x + y)
-
-print(int(x) + int(y))
